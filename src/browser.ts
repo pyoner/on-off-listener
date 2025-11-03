f9b2334a2ebf62b6d8d7bd2f185a6ae2a294f222
Askar Yusupov <devex.soft@gmail.com>
2025-11-03 10:38:18 +0500

    feat(browser): add delegate function

--- src/browser.ts
+++ src/browser.ts
@@ -1,2 +1,21 @@
 /// <reference path="types/generated/lib.dom.ts" />
 export * from "./index";
+
+export type DelegatedEvent<E extends Event> = E & { delegatedTarget: Element };
+
+export function delegate(selector: string) {
+  return function <
+    T extends <E extends Event>(
+      this: HTMLElement,
+      event: DelegatedEvent<E>,
+    ) => void,
+  >(fn: T) {
+    return function <E extends Event>(this: HTMLElement, event: E) {
+      const target = event.target as HTMLElement;
+      const delegatedTarget = target.closest(selector);
+      if (delegatedTarget) {
+        fn.call(this, { ...event, delegatedTarget });
+      }
+    };
+  };
+}
