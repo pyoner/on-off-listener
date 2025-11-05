ce34655367ab1ea8996264cc54e3c85413144781
Askar Yusupov <devex.soft@gmail.com>
2025-11-05 10:28:34 +0500

    chore(package.json): update project version from 0.1.0 to 0.2.0 to reflect new release refactor(browser.ts): enhance delegate function with generic types for better type safety and flexibility

--- src/browser.ts
+++ src/browser.ts
@@ -1,18 +1,42 @@
 /// <reference path="types/generated/lib.dom.ts" />
 export * from "./index";
 
-export type DelegatedEvent<E extends Event> = E & { delegatedTarget: Element };
+export type DelegatedEvent<E extends Event, T> = E & {
+  delegatedTarget: T;
+};
 
-export function delegate(selector: string) {
-  return function <
-    T extends <E extends Event>(
-      this: HTMLElement,
-      event: DelegatedEvent<E>,
-    ) => void,
-  >(fn: T) {
-    return function <E extends Event>(this: HTMLElement, event: E) {
+export type DelegatedListener<D extends Element> = <
+  T extends Element,
+  E extends Event,
+>(
+  this: T,
+  e: DelegatedEvent<E, D>,
+) => void;
+
+export type DelegatedWrapper<D extends Element> = (
+  fn: DelegatedListener<D>,
+) => <T extends Element, E extends Event>(this: T, e: E) => void;
+
+export function delegate<K extends keyof HTMLElementTagNameMap>(
+  selector: K,
+): DelegatedWrapper<HTMLElementTagNameMap[K]>;
+export function delegate<K extends keyof SVGElementTagNameMap>(
+  selector: K,
+): DelegatedWrapper<SVGElementTagNameMap[K]>;
+export function delegate<K extends keyof MathMLElementTagNameMap>(
+  selector: K,
+): DelegatedWrapper<MathMLElementTagNameMap[K]>;
+export function delegate<E extends Element = Element>(
+  selector: string,
+): DelegatedWrapper<E>;
+export function delegate<E extends Element = Element>(
+  selector: string,
+): DelegatedWrapper<E> {
+  return function (fn) {
+    return function (event) {
       const target = event.target as HTMLElement;
-      const delegatedTarget = target.closest(selector);
+      const delegatedTarget = target.closest<E>(selector);
+
       if (delegatedTarget) {
         fn.call(this, { ...event, delegatedTarget });
       }
