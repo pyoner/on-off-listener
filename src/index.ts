f9f62adfe73103d437e11b6dc81701411c3b69db
Askar Yusupov <devex.soft@gmail.com>
2025-11-14 10:49:11 +0500

    chore(package.json): update project version from 0.2.1 to 0.3.0 to reflect new release feat(src/index.ts): add TypeOptions type to enhance event listener options with type safety refactor(src/index.ts): modify 'on' function to accept flexible typeOptions parameter for better usability

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 import { eventMap } from "./property-key";
 
+export type TypeOptions<T extends string> = AddEventListenerOptions & {
+  type: T;
+};
+
 export function on<
   T extends EventTarget,
   K extends Extract<keyof T[typeof eventMap], string>,
 >(
   target: T,
-  type: K,
+  typeOptions: K | TypeOptions<K>,
   listener: (this: T, ev: T[typeof eventMap][K]) => void,
-  options?: boolean | AddEventListenerOptions,
 ): () => void;
 export function on<T extends EventTarget>(
   target: T,
-  type: string,
+  typeOptions: string | TypeOptions<string>,
   listener: EventListenerOrEventListenerObject,
-  options?: boolean | AddEventListenerOptions,
 ): () => void;
 export function on<T extends EventTarget>(
   target: T,
-  type: string,
+  typeOptions: string | TypeOptions<string>,
   listener: EventListenerOrEventListenerObject,
-  options?: boolean | AddEventListenerOptions,
 ) {
+  const { type, ...options } =
+    typeof typeOptions === "string" ? { type: typeOptions } : typeOptions;
   target.addEventListener(type, listener, options);
   return () => target.removeEventListener(type, listener, options);
 }
