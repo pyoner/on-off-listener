3684f5b0b77bfd6fdbfdf7b7d60173065328e928
Askar Yusupov <devex.soft@gmail.com>
2025-11-15 11:26:44 +0500

    refactor(index.ts): consolidate multiple overloads of the on function into a single implementation to improve code clarity and maintainability

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,49 @@ export type Listener<
 export function on<
   T extends EventTarget,
   K extends Extract<keyof T[typeof eventMap], string>,
->(target: T, typeOptions: K | TypeOptions<K>, listener: Listener<T, K>): Off;
-export function on<T extends EventTarget>(
+>(
   target: T,
-  typeOptions: string | TypeOptions<string>,
-  listener: EventListenerOrEventListenerObject,
-): Off;
-export function on<T extends EventTarget>(
+  typeOptions: K | TypeOptions<K>,
+): (listener: Listener<T, K>) => Off;
+export function on<
+  T extends EventTarget,
+  K extends Extract<keyof T[typeof eventMap], string>,
+  L extends Listener<T, K>,
+>(target: T, typeOptions: K | TypeOptions<K>, listener: L): Off;
+export function on<
+  T extends EventTarget,
+  K extends string | TypeOptions<string>,
+>(
   target: T,
-  typeOptions: string | TypeOptions<string>,
-  listener: EventListenerOrEventListenerObject,
-): Off {
-  const { type, ...options } =
-    typeof typeOptions === "string" ? { type: typeOptions } : typeOptions;
-  target.addEventListener(type, listener, options);
-  return () => target.removeEventListener(type, listener, options);
+  typeOptions: K,
+): (listener: EventListenerOrEventListenerObject) => Off;
+export function on<
+  T extends EventTarget,
+  K extends string | TypeOptions<string>,
+  L extends EventListenerOrEventListenerObject,
+>(target: T, typeOptions: K, listener: L): Off;
+export function on<
+  T extends EventTarget,
+  K extends string | TypeOptions<string>,
+  L extends EventListenerOrEventListenerObject,
+>(target: T, typeOptions: K, listener?: L): unknown {
+  let type: string;
+  let options: AddEventListenerOptions;
+
+  if (typeof typeOptions === "string") {
+    type = typeOptions;
+    options = {};
+  } else {
+    ({ type, ...options } = typeOptions);
+  }
+
+  if (listener === undefined) {
+    return (l: EventListenerOrEventListenerObject) => {
+      target.addEventListener(type, l, options);
+      return () => target.removeEventListener(type, l, options);
+    };
+  } else {
+    target.addEventListener(type, listener, options);
+    return () => target.removeEventListener(type, listener, options);
+  }
 }
