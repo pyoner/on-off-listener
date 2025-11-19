6fc5b44e653ec0095f5243de4b95072c5cac9444
Askar Yusupov <devex.soft@gmail.com>
2025-11-19 18:29:26 +0500

    chore(browser.ts): update export statements and enhance delegate function for better type safety and flexibility to support various element types and event delegation scenarios

--- src/browser.ts
+++ src/browser.ts
@@ -1,45 +1,96 @@
 /// <reference path="types/generated/lib.dom.ts" />
+
 export * from "./index";
 
-export type DelegatedEvent<E extends Event, T> = E & {
-  delegatedTarget: T;
-};
-
-export type DelegatedListener<D extends Element> = <
-  T extends Element,
-  E extends Event,
->(
+export type Listener<T extends EventTarget, E extends Event> = (
   this: T,
-  e: DelegatedEvent<E, D>,
+  e: E,
 ) => void;
 
-export type DelegatedWrapper<D extends Element> = (
-  fn: DelegatedListener<D>,
-) => <T extends Element, E extends Event>(this: T, e: E) => void;
+export type DelegatedEvent<T extends Element, E extends Event> = E & {
+  readonly delegatedTarget: T;
+};
 
-export function delegate<K extends keyof HTMLElementTagNameMap>(
-  selector: K,
-): DelegatedWrapper<HTMLElementTagNameMap[K]>;
-export function delegate<K extends keyof SVGElementTagNameMap>(
-  selector: K,
-): DelegatedWrapper<SVGElementTagNameMap[K]>;
-export function delegate<K extends keyof MathMLElementTagNameMap>(
-  selector: K,
-): DelegatedWrapper<MathMLElementTagNameMap[K]>;
-export function delegate<E extends Element = Element>(
-  selector: string,
-): DelegatedWrapper<E>;
-export function delegate<E extends Element = Element>(
-  selector: string,
-): DelegatedWrapper<E> {
-  return function (fn) {
-    return function (event) {
-      const target = event.target as HTMLElement;
-      const delegatedTarget = target.closest<E>(selector);
+export type DelegatedListener<T extends Element, E extends Event> = Listener<
+  T,
+  DelegatedEvent<T, E>
+>;
 
-      if (delegatedTarget) {
-        fn.call(this, Object.assign(event, { delegatedTarget }));
-      }
-    };
+export function delegate<
+  T extends EventTarget,
+  E extends Event,
+  K extends keyof HTMLElementTagNameMap,
+>(
+  selector: K,
+  listener: DelegatedListener<HTMLElementTagNameMap[K], E>,
+): Listener<T, E>;
+export function delegate<
+  T extends EventTarget,
+  E extends Event,
+  K extends keyof SVGElementTagNameMap,
+>(
+  selector: K,
+  listener: DelegatedListener<SVGElementTagNameMap[K], E>,
+): Listener<T, E>;
+export function delegate<
+  T extends EventTarget,
+  E extends Event,
+  K extends keyof MathMLElementTagNameMap,
+>(
+  selector: K,
+  listener: DelegatedListener<MathMLElementTagNameMap[K], E>,
+): Listener<T, E>;
+export function delegate<
+  T extends EventTarget,
+  E extends Event,
+  D extends Element = Element,
+>(selector: string, listener: DelegatedListener<D, E>): Listener<T, E>;
+export function delegate<
+  T extends EventTarget,
+  E extends Event,
+  D extends Element = Element,
+>(selector: string, listener: DelegatedListener<D, E>): Listener<T, E> {
+  return function (event) {
+    const target = event.target as HTMLElement;
+    const delegatedTarget = target.closest<D>(selector);
+
+    if (delegatedTarget) {
+      listener.call(
+        this,
+        Object.create(event, {
+          delegatedTarget: {
+            value: delegatedTarget,
+            writable: false,
+          },
+        }),
+      );
+    }
   };
 }
+
+const handler = delegate<HTMLBodyElement, MouseEvent>("a", (e) => {
+  e.target;
+  e.currentTarget;
+  e.delegatedTarget;
+  e.button;
+});
+
+declare function d<E extends Event, K extends keyof HTMLElementTagNameMap>(
+  s: K,
+  l: (e: E) => void,
+): (e: E) => void;
+document.addEventListener(
+  "click",
+  d("abbr", (e) => {
+    e.target;
+    e.button;
+  }),
+);
+
+document.addEventListener(
+  "click",
+  delegate("a", (e) => {
+    e.button;
+    e.delegatedTarget;
+  }),
+);
