546c87d68408707fed4b04a7d7d4c511d0542ca6
Askar Yusupov <devex.soft@gmail.com>
2025-11-21 11:13:28 +0500

    feat(filter): add closest filter

--- src/browser.ts
+++ src/browser.ts
@@ -1,131 +1,58 @@
 /// <reference path="types/generated/lib.dom.ts" />
 
-import { on } from "./index";
-
 export * from "./index";
 
 export type Listener<T extends EventTarget, E extends Event> = (
   this: T,
-  e: E,
+  event: E,
 ) => void;
 
-export type DelegatedEvent<T extends Element, E extends Event> = E & {
-  readonly delegatedTarget: T;
-};
-
-export type DelegatedListener<
+export type FilterListener<
   T extends EventTarget,
-  D extends Element,
   E extends Event,
-> = Listener<T, DelegatedEvent<D, E>>;
+  F = undefined,
+> = (this: T, event: E, value: F) => void;
 
-export function delegate<
+export function closest<
   T extends EventTarget,
   E extends Event,
   K extends keyof HTMLElementTagNameMap,
 >(
   selector: K,
-  listener: DelegatedListener<T, HTMLElementTagNameMap[K], E>,
+  listener: FilterListener<T, E, HTMLElementTagNameMap[K]>,
 ): Listener<T, E>;
-export function delegate<
+export function closest<
   T extends EventTarget,
   E extends Event,
   K extends keyof SVGElementTagNameMap,
 >(
   selector: K,
-  listener: DelegatedListener<T, SVGElementTagNameMap[K], E>,
+  listener: FilterListener<T, E, SVGElementTagNameMap[K]>,
 ): Listener<T, E>;
-export function delegate<
+export function closest<
   T extends EventTarget,
   E extends Event,
   K extends keyof MathMLElementTagNameMap,
 >(
   selector: K,
-  listener: DelegatedListener<T, MathMLElementTagNameMap[K], E>,
+  listener: FilterListener<T, E, MathMLElementTagNameMap[K]>,
 ): Listener<T, E>;
-export function delegate<
+export function closest<
   T extends EventTarget,
   E extends Event,
   D extends Element = Element,
->(selector: string, listener: DelegatedListener<T, D, E>): Listener<T, E>;
-export function delegate<
+>(selector: string, listener: FilterListener<T, E, D>): Listener<T, E>;
+export function closest<
   T extends EventTarget,
   E extends Event,
   D extends Element = Element,
->(selector: string, listener: DelegatedListener<T, D, E>): Listener<T, E> {
+>(selector: string, listener: FilterListener<T, E, D>): Listener<T, E> {
   return function (event) {
     const target = event.target as HTMLElement;
-    const delegatedTarget = target.closest<D>(selector);
+    const value = target.closest<D>(selector);
 
-    if (delegatedTarget) {
-      listener.call(
-        this,
-        Object.create(event, {
-          delegatedTarget: {
-            value: delegatedTarget,
-            writable: false,
-          },
-        }),
-      );
+    if (value) {
+      listener.call(this, event, value);
     }
   };
 }
-
-const handler = delegate<HTMLBodyElement, MouseEvent>("a", function (e) {
-  this;
-  e.target;
-  e.currentTarget;
-  e.delegatedTarget;
-  e.button;
-});
-
-declare function d<E extends Event, K extends keyof HTMLElementTagNameMap>(
-  s: K,
-  l: (e: E) => void,
-): (e: E) => void;
-document.addEventListener(
-  "click",
-  d("abbr", (e) => {
-    e.target;
-    e.button;
-  }),
-);
-
-document.addEventListener(
-  "click",
-  delegate("a", function (e) {
-    this;
-    e.button;
-    e.target;
-    e.delegatedTarget;
-  }),
-);
-
-on(document, "click", function (e) {
-  this;
-  e.button;
-  e.target;
-  e.delegatedTarget;
-});
-
-on(
-  document,
-  "click",
-  delegate("a", function (e) {
-    this;
-    e.button;
-    e.target;
-    e.delegatedTarget;
-  }),
-);
-
-on(
-  document,
-  { type: "click" },
-  delegate("a", function (e) {
-    this;
-    e.button;
-    e.target;
-    e.delegatedTarget;
-  }),
-);
